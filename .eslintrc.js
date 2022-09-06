3b437df4b8d3478a7574178342d148d6889f992b
sleonia <nikita.dontsov@yahoo.com>
2022-09-07 00:48:01 +0300

    chore: базовая настройка линтеров, tsconfig'а, npmrc, nvmrc

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+    extends: [
+        'eslint:recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier'
+    ],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module'
+    },
+    plugins: ['@typescript-eslint']
+}
