a627aeaf4dfa2617fe8323b93e8d99d8902c8023
Aleksandra <guseva-47@yandex.ru>
2022-09-08 03:50:16 +0700

    refactor: eslint fix

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
-import ReactDOM from 'react-dom/client'
-import { App } from './components/app'
-import './i18next'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import App from './components/app';
+import './i18next';
+import './index.css';
 
-const root = document.getElementById('root') as HTMLElement
+const root = document.getElementById('root') as HTMLElement;
 
-ReactDOM.createRoot(root).render(<App />)
+ReactDOM.createRoot(root).render(<App />);
